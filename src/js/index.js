f3af9f41e9dd4355a3496b200aa412447fee06c1
alexscoelho <alexsoncoelho21@hotmail.com>
2020-07-13 16:09:18 +0000

    improvements app

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,37 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+function domainGenerator() {
+  let pronoun = ["the", "our", "their", "mine", "the", "some", "whom"];
+  let adj = ["great", "big", "blue", "red", "small", "flat", "smart", "cool"];
+  let noun = ["jogger", "racoon", "ant", "flower", "rabbit", "table"];
+  let ext = [".net", ".com", ".edu"];
+  let domain = [];
+
+  for (let h = 0; h < pronoun.length; h++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < ext.length; l++) {
+          domain.push(pronoun[h] + adj[j] + noun[k] + ext[l]);
+        }
+      }
+    }
+  }
+
+  var domainList = document.querySelector(".domain-list");
+  domain.forEach(item => {
+    var li = document.createElement("li");
+    li.innerHTML = item;
+    domainList.appendChild(li);
+  });
+}
+
+// function sorter() {
+//   domain.sort();
+// }
+
+var button = document.querySelector(".generator");
+button.addEventListener("click", domainGenerator);
+
+// var sort = document.querySelector(".sort");
+// sort.addEventListener("click", sorter);
